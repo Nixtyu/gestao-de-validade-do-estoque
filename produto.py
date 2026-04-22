88ece814072db8e07c64dabad21b017fb15f7d8b
Nixtyu <marlond.candido28@gmail.com>
2026-04-21 21:53:25 -0300

    Update produto.py

--- produto.py
+++ produto.py
@@ -12,12 +12,50 @@ class Produto:
         diferenca = self.data_validade - hoje
         return diferenca.days
 
-# Simulação:
 estoque_mercearia = [
     # Repare: Mesmo produto, mas LOTES e VALIDADES diferentes
     Produto("Ovo maltine 190g", "01/05/2026", "LOTE-A1", "Matinal"),
     Produto("Nesquik", "01/06/2026", "L22", "Matinal"),
     Produto("Sucrilos Kellogs Power pops", "23/06/2026", "G90", "Matinal")
+    Produto("nestle snow 620g", "27/07/2026", "lote 1", "Matinal")
+    # Quatá
+    Produto("Quata", "08/05/2026", "Lote 1", "Laticínios/Matinal"),
+    Produto("Quata", "11/05/2026", "Lote 2", "Laticínios/Matinal"),
+    Produto("Quata", "13/07/2026", "Lote 3", "Laticínios/Matinal"),
+    
+    # Bebidas e Chás
+    Produto("Cha Leao Guarana Garrafa", "29/04/2026", "Lote 1", "Matinal"),
+    
+    # Chocolates e Wafers
+    Produto("Kinder Joy", "31/05/2026", "Lote Único", "Chocolates"),
+    Produto("Chocolate Arcor Brigadeiro", "21/05/2026", "Lote Único", "Chocolates"),
+    Produto("70% Cacau Cafe Espresso", "26/05/2026", "Lote Único", "Chocolates"),
+    Produto("70% Cacau Caramel Macchiato", "25/05/2026", "Lote Único", "Chocolates"),
+    Produto("Chocolate Lacta 40% Cacau", "30/05/2026", "Lote Único", "Chocolates"),
+    Produto("Piraque Chocowafer", "02/05/2026", "Lote Único", "Chocolates"),
+    
+    # Cremes de Leite
+    Produto("Porto Alegre Creme de Leite", "29/06/2026", "Lote 1", "Mercearia"),
+    Produto("Porto Alegre Creme de Leite", "15/07/2026", "Lote 2", "Mercearia"),
+    Produto("Leco Creme de Leite", "19/06/2026", "Lote Único", "Mercearia"),
+    Produto("Camponesa Creme de Leite", "18/05/2026", "Lote 1", "Mercearia"),
+    Produto("Camponesa Creme de Leite", "15/06/2026", "Lote 2", "Mercearia"),
+    
+    # Matinais e Cereais
+    Produto("Nestle Snow 230g", "06/07/2026", "Lote Único", "Matinal"),
+    Produto("Aveia em Flocos Yoki", "10/07/2026", "Lote Único", "Matinal"),
+    Produto("Quaker Flocos Finos", "05/07/2026", "Lote Único", "Matinal"),
+    Produto("Viver Bem Aveia Flocos Grossos 500g", "25/07/2026", "Lote Único", "Produtos Light"),
+    
+    # Mucilon
+    Produto("Mucilon 600g Arroz e Aveia", "01/05/2026", "Lote 1", "Matinal"),
+    Produto("Mucilon 600g Arroz e Aveia", "01/07/2026", "Lote 2", "Matinal"),
+    Produto("Mucilon 600g Arroz e Aveia", "01/08/2026", "Lote 3", "Matinal"),
+    Produto("Mucilon Multicereais 600g", "01/05/2026", "Lote 1", "Matinal"),
+    Produto("Mucilon Multicereais 600g", "01/09/2026", "Lote 2", "Matinal"),
+    
+    # Torradas e Light
+    Produto("Torrada Adria Light", "15/05/2026", "Lote Único", "Produtos Light")
 ]
 
 print(f"--- RELATÓRIO SETOR: MERCEARIA ---")
